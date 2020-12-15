8ed70bdb8364aea8845b19629b22fff9d93ac219
Rulllan <ryllian@mail.ru>
2020-12-16 00:11:03 +0500

    Home6

--- src/Homework6/Animals.java
+++ src/Homework6/Animals.java
@@ -1,4 +1,29 @@
 package HomeWork6;
 
-public class Animals {
+public abstract class Animals<run> {
+    protected String name;
+    private String Color;
+    protected int Age;
+
+    public Animals (String name, String color, int age) {
+        this.name = name;
+        Color = color;
+        Age = age;
+    }
+
+
+
+
+  /*  public void run1 (int high) {
+    }
+    public swim (int long) {
+    }*/
+    @Override
+    public String toString () {
+        return "Animals{" +
+                "name='" + name + '\'' +
+                ", Color='" + Color + '\'' +
+                ", Age=" + Age +
+                '}';
+    }
 }
--- src/Homework6/Cats.java
+++ src/Homework6/Cats.java
@@ -1,4 +1,34 @@
 package HomeWork6;
 
-public class Cats {
+public class Cats extends Animals{
+    protected static int longswim = 0;
+    protected static int longjamp = 2;
+    protected static int longRun = 200;
+
+    public Cats (String name, String color, int age) {
+        super (name, color, age);
+    }
+    public boolean Run (int i) {
+        if(i<this.longRun){
+            return  true;
+        }
+        return false;
+    }
+
+    public boolean Jamp (double i) {
+        if(i<this.longjamp){
+            return  true;
+        }
+        return false;
+    }
+
+    public boolean swim (int i) {
+        if(i<this.longswim){
+            return  true;
+        }
+        return false;
+    }
+
+
 }
+
